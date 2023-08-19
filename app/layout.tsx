7f060b4d38caf99fc1329f3a66b05dd97fb74736
rplusq <rafaplusq@gmail.com>
2023-08-19 22:17:27 +0100

    feature: google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,16 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body className={cn(inter.className, "h-full")}>{children}</body>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-JHEBLEJR58" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-JHEBLEJR58');
+        `}
+      </Script>
     </html>
   );
 }
